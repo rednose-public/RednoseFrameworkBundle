3bd5f52a24ffeb51338cfb7708ba697f30dcce7a
Sven Hagemann <sven@rednose.nl>
2012-06-15 16:03:23 +0200

    User creation fix

--- Security/UserManager.php
+++ Security/UserManager.php
@@ -18,10 +18,34 @@ class UserManager extends BaseUserManager
             $user = $this->createUser();
             $user->setUserName($username);
             $user->setEnabled(true);
-
+            $user->setEmail($username);
+            $user->setPassword($this->randomPassword());
             $this->updateUser($user);
         }
 
         return parent::loadUserByUsername($username);
+    }
+    
+    public function randomPassword($length = 9)
+    {
+        $vowels = 'aeuy';
+
+        $consonants = 'bdghjmnpqrstvz';
+        $consonants .= '@#$%';
+     
+        $password = '';
+        $alt = time() % 2;
+        
+        for ($i = 0; $i < $length; $i++) {
+            if ($alt == 1) {
+                $password .= $consonants[(rand() % strlen($consonants))];
+                $alt = 0;
+            } else {
+                $password .= $vowels[(rand() % strlen($vowels))];
+                $alt = 1;
+            }
+        }
+        
+        return $password;
     }
 }
