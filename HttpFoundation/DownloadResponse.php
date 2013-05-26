36ce69f6e727f6497361e3bfa8955252238a4fed
Marc Bontje <marc@rednose.nl>
2013-05-26 12:21:26 +0200

    Added generic download response

---
+++ HttpFoundation/DownloadResponse.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Libbit\FrameworkBundle\HttpFoundation;
+
+use Symfony\Component\HttpFoundation\Response;
+
+/**
+ * Injects the headers needed for a file download.
+ *
+ * @author Marc Bontje <marc@rednose.nl>
+ */
+class DownloadResponse extends Response
+{
+    /**
+     * Constructor
+     *
+     * @param string $content  The binary file contents
+     * @param string $filename The file name
+     * @param string $mimetype Mimetype
+     */
+    public function __construct($content = '', $filename = '', $mimetype = 'application/octet-stream')
+    {
+        $headers = array(
+            'Content-Type' => $mimetype,
+            'Content-Disposition' => 'filename="'.$filename.'"',
+        );
+
+        parent::__construct($content, 200, $headers);
+    }
+}
