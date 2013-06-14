21bb6fcf2df3721d30166409052257532f82776e
Marc Bontje <marc@rednose.nl>
2013-06-14 11:25:39 +0200

    Revert "Model pattern"
    This reverts commit 643f0841de959c8b996c48b4a78c74406b8fa706.

---
+++ Entity/User.php
@@ -0,0 +1,90 @@
+<?php
+
+namespace Libbit\FrameworkBundle\Entity;
+
+use FOS\UserBundle\Entity\User as BaseUser;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * A LiBBiT framework user
+ *
+ * @ORM\Entity
+ * @ORM\Table(name="libbit_framework_user")
+ */
+class User extends BaseUser
+{
+    const ROLE_ADMIN = 'ROLE_ADMIN';
+
+    /**
+     * @ORM\Id
+     * @ORM\Column(type="integer")
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    protected $id;
+
+    /**
+     *
+     * @ORM\ManyToMany(targetEntity="Libbit\FrameworkBundle\Entity\Group")
+     *
+     * @ORM\JoinTable(name="libbit_framework_user_group",
+     *   joinColumns={@ORM\JoinColumn(name="user_id", referencedColumnName="id")},
+     *   inverseJoinColumns={@ORM\JoinColumn(name="group_id", referencedColumnName="id")}
+     * )
+     */
+    protected $groups;
+
+    /**
+     * Gets the username
+     *
+     * Will automaticly return the username in lowercase for
+     * framework compatibility.
+     *
+     * if forceLowercase is set to false it will return the
+     * username as it has been set by setUsername().
+     *
+     * @param $forceLowercase
+     * @return string
+     */
+    public function getUsername($forceLowercase = true)
+    {
+        if ($forceLowercase) {
+            return strtolower($this->username);
+        }
+
+        return $this->username;
+    }
+
+    public function getExpiresAt()
+    {
+        return $this->expiresAt;
+    }
+
+    public function getCredentialsExpireAt()
+    {
+        return $this->credentialsExpireAt;
+    }
+
+    /**
+     * Tells if the the given user has super admin role.
+     *
+     * @return Boolean
+     */
+    public function isAdmin()
+    {
+        return $this->hasRole(static::ROLE_ADMIN);
+    }
+
+    /**
+     * Sets the admin status
+     *
+     * @param Boolean $boolean
+     */
+    public function setAdmin($boolean)
+    {
+        if (true === $boolean) {
+            $this->addRole(static::ROLE_ADMIN);
+        } else {
+            $this->removeRole(static::ROLE_ADMIN);
+        }
+    }
+}
