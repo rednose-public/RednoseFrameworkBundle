b57ef98b7440e7697fb06072dff65d936f5a9ff0
Marc Bontje <marc@rednose.nl>
2013-10-10 17:33:30 +0200

    Basic push device subscription for iOS devices

---
+++ Controller/DeviceController.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace Rednose\FrameworkBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Response;
+use Rednose\FrameworkBundle\Entity\Device;
+
+class DeviceController extends Controller
+{
+    public function iosSubscribeAction()
+    {
+        $em = $this->get('doctrine.orm.entity_manager');
+
+        $identifier = $this->get('request')->query->get('device_identifier');
+        $token      = $this->get('request')->query->get('device_token');
+        $user       = $this->get('security.context')->getToken()->getUser();
+
+        $device = $em->getRepository('Rednose\FrameworkBundle\Entity\Device')->findOneBy(array('user' => $user, 'identifier' => $identifier));
+
+        if ($device === null) {
+            $device = new Device;
+
+            $device->setType(Device::TYPE_IOS);
+
+            $device->setUser($user);
+            $device->setIdentifier($identifier);
+        }
+
+        $device->setToken($token);
+
+        $em->persist($device);
+        $em->flush();
+
+        return new Response();
+    }
+
+    public function iosUnsubscribeAction()
+    {
+        $em = $this->get('doctrine.orm.entity_manager');
+
+        $identifier = $this->get('request')->query->get('device_identifier');
+        $user       = $this->get('security.context')->getToken()->getUser();
+
+        $devices = $em->getRepository('Rednose\FrameworkBundle\Entity\Device')->findBy(array('user' => $user, 'identifier' => $identifier));
+
+        foreach ($devices as $device) {
+            $em->remove($device);
+        }
+
+        $em->flush();
+
+        return new Response();
+    }
+}
---
+++ Entity/Device.php
@@ -0,0 +1,137 @@
+<?php
+
+namespace Rednose\FrameworkBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use Doctrine\Common\Collections\ArrayCollection;
+use Symfony\Component\Validator\Constraints as Assert;
+use Rednose\FrameworkBundle\Entity\User;
+use DateTime;
+
+/**
+ * @ORM\Entity()
+ * @ORM\Table(name="rednose_framework_device")
+ * @ORM\HasLifecycleCallbacks()
+ */
+class Device
+{
+    const TYPE_IOS = 'ios';
+
+    /**
+     * @ORM\Id
+     * @ORM\Column(type="integer")
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    protected $id;
+
+    /**
+     * @ORM\ManyToOne(targetEntity="Rednose\FrameworkBundle\Entity\User")
+     *
+     * @ORM\JoinColumn(
+     *   name="owner_id",
+     *   referencedColumnName="id",
+     *   onDelete="CASCADE")
+     */
+    protected $user;
+
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    protected $createdAt;
+
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    protected $modifiedAt;
+
+    /**
+     * @ORM\Column(type="string")
+     */
+    protected $type;
+
+    /**
+     * @ORM\Column(type="string")
+     */
+    protected $identifier;
+
+    /**
+     * @ORM\Column(type="string")
+     */
+    protected $token;
+
+    // -- Getters and Setters --------------------------------------------------
+
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    public function getUser()
+    {
+        return $this->user;
+    }
+
+    public function setUser(User $user)
+    {
+        $this->user = $user;
+    }
+
+    public function getCreatedAt()
+    {
+        return $this->createdAt;
+    }
+
+    public function getModifiedAt()
+    {
+        return $this->createdAt;
+    }
+
+    public function getType()
+    {
+        return $this->type;
+    }
+
+    public function setType($type)
+    {
+        $this->type = $type;
+    }
+
+    public function getIdentifier()
+    {
+        return $this->identifier;
+    }
+
+    public function setIdentifier($identifier)
+    {
+        $this->identifier = $identifier;
+    }
+
+    public function getToken()
+    {
+        return $this->token;
+    }
+
+    public function setToken($token)
+    {
+        $this->token = $token;
+    }
+
+    // -- Lifecycle Callback Methods -------------------------------------------
+
+    /**
+     * @ORM\PrePersist
+     */
+    public function setCreatedAtValue()
+    {
+        $this->createdAt  = new DateTime();
+        $this->modifiedAt = new DateTime();
+    }
+
+    /**
+     * @ORM\PreUpdate
+     */
+    public function setModifiedAtValue()
+    {
+        $this->modifiedAt = new DateTime();
+    }
+}
