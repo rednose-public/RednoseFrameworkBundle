a9b30f4b6a191e5135845a54c410ab138750a145
Marc Bontje <marc@rednose.nl>
2013-12-06 19:21:54 +0100

    Form label extension

---
+++ Form/Extension/LabelFormTypeExtension.php
@@ -0,0 +1,39 @@
+<?php
+
+/*
+ * This file is part of the RednoseDocgenClientBundle package.
+ *
+ * (c) RedNose <info@rednose.nl>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Rednose\DocgenClientBundle\Form\Extension;
+
+use Symfony\Component\Form\AbstractTypeExtension;
+use Symfony\Component\Form\FormBuilderInterface;
+
+/**
+ * Allows disabling of form labels, by setting them to false
+ */
+class LabelFormTypeExtension extends AbstractTypeExtension
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        if (array_key_exists('label', $options) && $options['label'] === false) {
+            $builder->setAttribute('label', false);
+        }
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getExtendedType()
+    {
+        return 'field';
+    }
+}
