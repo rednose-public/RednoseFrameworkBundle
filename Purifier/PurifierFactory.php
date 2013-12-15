af1d08e408bc67eb71ac149b2158481779e63529
Marc Bontje <marc@rednose.nl>
2013-12-15 16:51:52 +0100

    Added HTML purifier service

---
+++ Purifier/PurifierFactory.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Rednose\FrameworkBundle\Purifier;
+
+use HTMLPurifier;
+use HTMLPurifier_Bootstrap;
+use HTMLPurifier_Config;
+
+/**
+ * Factory class that constructs a configured HTML purifier instance.
+ */
+class PurifierFactory
+{
+    /**
+     * Creates and returns a purifier instance.
+     *
+     * @return HTMLPurifier
+     */
+    public function create()
+    {
+        new HTMLPurifier_Bootstrap;
+
+        $cache = sys_get_temp_dir() . '/fgHtmlPurify';
+
+        if (is_dir($cache) === false) {
+            mkdir($cache, 0777, true);
+            umask(umask(0));
+        }
+
+        $config = HTMLPurifier_Config::createDefault();
+
+        $config->set('Cache.SerializerPath', $cache);
+        $config->set('CSS.AllowedProperties', array());
+
+        $def = $config->getHTMLDefinition(true);
+
+        // Add textblock placeholder attribute to allowed definitions.
+        $def->addAttribute('span', 'data-placeholder', 'Text');
+
+        return new HTMLPurifier($config);
+    }
+}
