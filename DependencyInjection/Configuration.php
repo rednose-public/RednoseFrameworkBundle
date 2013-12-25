4b38bbfe40df7ec75abec117b2b5a98d383bce4c
Marc Bontje <marc@rednose.nl>
2013-12-25 14:26:40 +0100

    Made Oauth support optional in the framework

---
+++ DependencyInjection/Configuration.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Rednose\FrameworkBundle\DependencyInjection;
+
+use Symfony\Component\Config\Definition\Builder\TreeBuilder;
+use Symfony\Component\Config\Definition\Builder\ArrayNodeDefinition;
+use Symfony\Component\Config\Definition\ConfigurationInterface;
+
+
+/**
+ * Configuration for this bundle.
+ */
+class Configuration implements ConfigurationInterface
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function getConfigTreeBuilder()
+    {
+        $treeBuilder = new TreeBuilder();
+        $rootNode    = $treeBuilder->root('rednose_framework', 'array');
+
+        $this->addOauthSection($rootNode);
+
+        return $treeBuilder;
+    }
+
+    private function addOauthSection(ArrayNodeDefinition $node)
+    {
+        $node
+            ->children()
+                ->booleanNode('oauth')->defaultFalse()->end()
+            ->end();
+    }
+}
--- DependencyInjection/RednoseFrameworkExtension.php
+++ DependencyInjection/RednoseFrameworkExtension.php
@@ -6,6 +6,7 @@ use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 use Symfony\Component\DependencyInjection\Loader\XmlFileLoader;
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\Config\Definition\Processor;
 
 /**
  * Loads the configuration files and injects them to the service container.
@@ -19,7 +20,16 @@ class RednoseFrameworkExtension extends Extension
      */
     public function load(array $configs, ContainerBuilder $container)
     {
+        $processor = new Processor();
+        $configuration = new Configuration();
+
+        $config = $processor->processConfiguration($configuration, $configs);
+
         $loader = new XmlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
+
+        if ($config['oauth']) {
+            $loader->load('oauth.xml');
+        }
 
         $serviceFiles = array('admin', 'orm', 'services');
 
