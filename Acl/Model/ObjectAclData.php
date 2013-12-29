64372a4f78277d748a2e87e8958de7a97b19dbfd
Marc Bontje <marc@rednose.nl>
2013-12-29 17:17:46 +0100

    Abstracted ACL manipulator service

---
+++ Acl/Model/ObjectAclData.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace Rednose\FrameworkBundle\Acl\Model;
+
+class ObjectAclData
+{
+    /**
+     * @var object
+     */
+    protected $object;
+
+    /**
+     * @var array
+     */
+    protected $identities;
+
+    /**
+     * @var array
+     */
+    protected $permissions;
+
+    /**
+     * @var array
+     */
+    protected $data;
+
+    /**
+     * Constructor.
+     *
+     * @param object $object
+     * @param array  $identities
+     * @param array  $permissions
+     */
+    public function __construct($object, array $identities, array $permissions)
+    {
+        $this->object      = $object;
+        $this->identities  = $identities;
+        $this->permissions = $permissions;
+    }
+
+    /**
+     * @return object
+     */
+    public function getObject()
+    {
+        return $this->object;
+    }
+
+    /**
+     * @return object[]
+     */
+    public function getIdentities()
+    {
+        return $this->identities;
+    }
+
+    /**
+     * @return string[]
+     */
+    public function getPermissions()
+    {
+        return $this->permissions;
+    }
+
+    /**
+     * @return array
+     */
+    public function getData()
+    {
+        return $this->data ? $this->data : array();
+    }
+
+    /**
+     * @param array $data
+     */
+    public function setData(array $data)
+    {
+        $this->data = $data;
+    }
+}
