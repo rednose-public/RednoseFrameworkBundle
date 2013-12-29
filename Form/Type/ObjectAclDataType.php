80b0b78dcb6cf314aa8f7803565110e797d5cbb4
Marc Bontje <marc@rednose.nl>
2013-12-29 18:15:25 +0100

    Updated and refactored the ACL implementation

---
+++ Form/Type/ObjectAclDataType.php
@@ -0,0 +1,42 @@
+<?php
+
+/*
+ * This file is part of the RednoseFrameworkBundle package.
+ *
+ * (c) RedNose <info@rednose.nl>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Rednose\FrameworkBundle\Form\Type;
+
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\AbstractType;
+
+class ObjectAclDataType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        if (!isset($options['data'])) {
+            throw new \InvalidArgumentException('Data needs to be passed on form type construction');
+        }
+
+        // TODO: Implement datatransformer
+        foreach ($options['data'] as $identity => $entry) {
+            foreach ($entry as $permission => $value) {
+                $builder->add($identity.$permission, 'checkbox', array('required' => false, 'data' => $value));
+            }
+        }
+    }
+
+    public function getDefaultOptions(array $options)
+    {
+        return array('data_class' => null);
+    }
+
+    public function getName()
+    {
+        return 'rednose_framework_acl';
+    }
+}
