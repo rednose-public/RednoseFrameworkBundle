c5b66bf3fc29b52c1cc80bb2d3849c324a941a36
Sven Hagemann <sven@rednose.nl>
2014-04-29 14:41:43 +0200

    Added the ability to get all clientKeys

--- Entity/ClientManager.php
+++ Entity/ClientManager.php
@@ -7,6 +7,11 @@ use FOS\OAuthServerBundle\Entity\ClientManager as BaseClientManager;
 
 class ClientManager extends BaseClientManager
 {
+    public function findClientsBy(array $criteria)
+    {
+        return $this->repository->findBy($criteria);
+    }
+
     public function findClientByPublicId($publicId)
     {
         return $this->findClientBy(array(
