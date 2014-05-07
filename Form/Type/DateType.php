7b55c33099eed45c2c0f6961c274d5b128469fd9
Marc Bontje <marc@rednose.nl>
2014-05-07 20:08:37 +0200

    Updated license headers

--- Form/Type/DateType.php
+++ Form/Type/DateType.php
@@ -3,7 +3,7 @@
 /*
  * This file is part of the RednoseFrameworkBundle package.
  *
- * (c) RedNose <info@rednose.nl>
+ * (c) RedNose <http://www.rednose.nl>
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
--- Model/Node/Value/OutputValueNodeInterface.php
+++ Model/Node/Value/OutputValueNodeInterface.php
@@ -1,5 +1,14 @@
 <?php
 
+/*
+ * This file is part of the RednoseFrameworkBundle package.
+ *
+ * (c) RedNose <http://www.rednose.nl>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 namespace Rednose\FrameworkBundle\Model\Node\Value;
 
 use Rednose\FrameworkBundle\Model\Node\OutputNodeInterface;
