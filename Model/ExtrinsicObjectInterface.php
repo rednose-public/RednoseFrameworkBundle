2c7b31893bcf357d480649d59e46b5052035e862
Marc Bontje <marc@rednose.nl>
2014-06-11 17:08:11 +0200

    Updated data model for AssetInterface

--- Model/ExtrinsicObjectInterface.php
+++ Model/ExtrinsicObjectInterface.php
@@ -13,17 +13,4 @@ namespace Rednose\FrameworkBundle\Model;
 
 interface ExtrinsicObjectInterface
 {
-    /**
-     * Returns the id.
-     *
-     * @return string $id
-     */
-    function getForeignId();
-
-    /**
-     * Sets the id.
-     *
-     * @param string $id
-     */
-    function setForeignId($id);
 }
