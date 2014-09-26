0d70fb309f4496b02be7ca01e56fffcc1b4ec44c
Marc Bontje <marc@rednose.nl>
2014-09-26 13:22:08 +0200

    Cleanup, removed container injection from loader

--- Serializer/Construction/DoctrineObjectConstructor.php
+++ Serializer/Construction/DoctrineObjectConstructor.php
@@ -66,58 +66,6 @@ class DoctrineObjectConstructor implements ObjectConstructorInterface
             }
         }
 
-        $class = new \ReflectionClass($metadata->name);
-
-        // TODO: Remove
-        if ($class->implementsInterface('Rednose\FrameworkBundle\Model\ExtrinsicObjectInterface') && $context->getFormat() === 'xml') {
-            // Try to find an id within the attributes that could hold a foreignId
-            foreach ($data->attributes() as $k => $v) {
-                if ($k === 'id') {
-                    $object = $objectManager->getRepository($metadata->name)->findOneByForeignId($v);
-
-                    if ($object) {
-                        $object = $objectManager->find($metadata->name, array('id' => $object->getId()));
-
-                        $objectManager->initializeObject($object);
-
-                        return $object;
-                    }
-                }
-            }
-        }
-
         return $this->fallbackConstructor->construct($visitor, $metadata, $data, $type, $context);
-//        // Locate possible ClassMetadata
-//        $classMetadataFactory = $objectManager->getMetadataFactory();
-//
-//        if ($classMetadataFactory->isTransient($metadata->name)) {
-//            // No ClassMetadata found, proceed with normal deserialization
-//            return $this->fallbackConstructor->construct($visitor, $metadata, $data, $type, $context);
-//        }
-
-        // Managed entity, check for proxy load
-        // if (!is_array($data)) {
-        //     // Single identifier, load proxy
-        //     return $objectManager->getReference($metadata->name, $data);
-        // }
-
-        // Fallback to default constructor if missing identifier(s)
-//        $classMetadata  = $objectManager->getClassMetadata($metadata->name);
-//        $identifierList = array();
-//
-//        foreach ($classMetadata->getIdentifierFieldNames() as $name) {
-//            if ( ! array_key_exists($name, $data)) {
-//                return $this->fallbackConstructor->construct($visitor, $metadata, $data, $type, $context);
-//            }
-//
-//            $identifierList[$name] = $data[$name];
-//        }
-//
-//        // Entity update, load it from database
-//        $object = $objectManager->find($metadata->name, $identifierList);
-//
-//        $objectManager->initializeObject($object);
-//
-//        return $object;
     }
 }
