5f1e23a69d726b2c81e7907267a4f53e7756e69a
Marc Bontje <marc@rednose.nl>
2014-10-01 10:44:34 +0200

    Xpath util

---
+++ Util/XpathUtil.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Rednose\FrameworkBundle\Util;
+
+/**
+ * Several XPath utility methods.
+ */
+class XpathUtil
+{
+    /**
+     * Gets a single node at a given xpath location, within an optional context.
+     *
+     * @param \DOMDocument $data
+     * @param string       $location
+     * @param \DOMNode     $context
+     *
+     * @return \DOMNode
+     */
+    public static function getXpathNode(\DOMDocument $data, $location, $context = null)
+    {
+        $xpath = new \DOMXPath($data);
+
+        $result = $xpath->query($location, $context);
+
+        if ($result->length > 0) {
+            $node = $result->item(0);
+
+            return $node;
+        }
+
+        return null;
+    }
+}
