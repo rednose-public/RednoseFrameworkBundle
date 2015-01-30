923f4f8e0625c21126b6182d1271f558cd0a5f1c
Sven Hagemann <sven@rednose.nl>
2015-01-30 14:20:34 +0100

    Added SsoState entity

---
+++ Entity/SsoState.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Rednose\FrameworkBundle\Entity;
+
+use AerialShip\SamlSPBundle\Entity\SSOStateEntity;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="rednose_framework_sso_state")
+ */
+class SsoState extends SSOStateEntity
+{
+    /**
+     * @ORM\Id
+     * @ORM\Column(type="guid")
+     * @ORM\GeneratedValue(strategy="UUID")
+     */
+    protected $id;
+
+    /**
+     * @param string $id
+     */
+    public function setId($id) {
+        $this->id = $id;
+    }
+
+    /**
+     * @return string
+     */
+    public function getId() {
+        return $this->id;
+    }
+}
