32dc96c52539df5fe201831f6187247775507529
Marc Bontje <marc@rednose.nl>
2015-02-17 22:04:07 +0100

    Added Xpath utility method

--- Util/XpathUtil.php
+++ Util/XpathUtil.php
@@ -34,4 +34,33 @@ class XpathUtil
 
         return null;
     }
+
+    /**
+     * @param \DOMDocument $data
+     * @param string       $query
+     * @param \DOMNode     $context
+     *
+     * @return mixed
+     */
+    public static function evaluate(\DOMDocument $data, $query, $context = null)
+    {
+        // Quickfix to avoid the need for single quote escaping.
+        if (substr($query, 0, 1) === "'" && substr($query, -1, 1) === "'") {
+            return trim($query, "'");
+        }
+
+        $xpath = new \DOMXPath($data);
+
+        $result = $xpath->evaluate($query, $context);
+
+        if ($result === false) {
+            throw new \InvalidArgumentException(sprintf('Invalid XPath expression: `%s`', $query));
+        }
+
+        if ($result instanceof \DOMNodeList && $result->length > 0) {
+            return $result->item(0)->nodeValue;
+        }
+
+        return $result;
+    }
 }
