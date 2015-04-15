c5bccf9c7ef8158df8b10861ff15a6d90a86f4cf
Marc Bontje <marc@rednose.nl>
2015-04-15 21:28:27 +0200

    Added dictionary nav list

--- Controller/DataDictionaryController.php
+++ Controller/DataDictionaryController.php
@@ -2,14 +2,15 @@
 
 namespace Rednose\FrameworkBundle\Controller;
 
-use FOS\RestBundle\View\View;
-use JMS\Serializer\SerializationContext;
-use JMS\Serializer\DeserializationContext;
-use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\HttpFoundation\JsonResponse;
 use FOS\RestBundle\Controller\Annotations as Rest;
+use FOS\RestBundle\Request\ParamFetcherInterface;
 use FOS\RestBundle\Util\Codes;
+use FOS\RestBundle\View\View;
+use JMS\Serializer\DeserializationContext;
+use JMS\Serializer\SerializationContext;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
 
 class DataDictionaryController extends Controller
 {
@@ -62,10 +63,13 @@ class DataDictionaryController extends Controller
 
     /**
      * @Rest\Get("/dictionaries", name="rednose_framework_get_dictionaries", options={"expose"=true})
+     *
+     * @Rest\QueryParam(name="organization_id", nullable=true, strict=true)
      */
-    public function getDictionariesAction()
+    public function getDictionariesAction(ParamFetcherInterface $paramFetcher)
     {
-        $organization = $this->get('request')->get('organization');
+        // 'organization' param is deprecated, use 'organization_id'
+        $organization = $paramFetcher->get('organization_id') ?: $this->get('request')->get('organization');
 
         if ($organization !== null) {
             $organization = $this->get('rednose_framework.organization_manager')->findOrganizationById($organization);
