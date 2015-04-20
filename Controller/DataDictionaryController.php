433eb53accb08fb5bcb64a2d9baafd3be453ec30
Marc Bontje <marc@rednose.nl>
2015-04-20 22:21:56 +0200

    DataDictionaryManager: Active dictionary, CRUD ops

--- Controller/DataDictionaryController.php
+++ Controller/DataDictionaryController.php
@@ -8,12 +8,33 @@ use FOS\RestBundle\Util\Codes;
 use FOS\RestBundle\View\View;
 use JMS\Serializer\DeserializationContext;
 use JMS\Serializer\SerializationContext;
+use Rednose\DocgenBundle\Util\XmlUtil;
+use Rednose\FrameworkBundle\DataDictionary\DataDictionaryInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\File\UploadedFile;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
 
 class DataDictionaryController extends Controller
 {
+    /**
+     * @Rest\Post("/dictionaries/{id}/activate", name="rednose_framework_activate_dictionary")
+     */
+    public function activateDictionaryAction($id)
+    {
+        $manager = $this->get('rednose_framework.data_dictionary_manager');
+        $dictionary = $manager->findDictionaryById($id);
+
+        if ($dictionary === null) {
+            throw $this->createNotFoundException();
+        }
+
+        $dictionary->getOrganization()->setDataDictionary($dictionary);
+        $manager->updateDictionary($dictionary);
+
+        return new Response(null, Codes::HTTP_NO_CONTENT);
+    }
+
     /**
      * @Rest\Post("/dictionaries", name="rednose_framework_post_dictionary",  options={"expose"=true})
      * @Rest\Put("/dictionaries/{id}", name="rednose_framework_put_dictionary",  options={"expose"=true})
@@ -52,19 +73,6 @@ class DataDictionaryController extends Controller
 
         $err = $manager->updateDictionary($dictionary, true);
 
-        // Bind the dictionary to a supplied organization. Warning: existing connection will be lost.
-        if ($err !== true) {
-            return new JsonResponse(array('message' => (string)$err), Codes::HTTP_BAD_REQUEST);
-        }
-
-        if ($organization !== null) {
-            if ($organization = $organizationManager->findOrganizationById($organization)) {
-                $organization->setDataDictionary($dictionary);
-
-                $organizationManager->updateOrganization($organization);
-            }
-        }
-
         return new JsonResponse(array('id' => $dictionary->getId()), Codes::HTTP_CREATED);
     }
 
@@ -101,11 +109,10 @@ class DataDictionaryController extends Controller
     public function getDictionaryAction($id)
     {
         $manager = $this->get('rednose_framework.data_dictionary_manager');
-
         $dictionary = $manager->findDictionaryById($id);
 
         if ($dictionary === null) {
-            return new Response(null, Codes::HTTP_NOT_FOUND);
+            throw $this->createNotFoundException();
         }
 
         $context = new SerializationContext();
@@ -119,6 +126,22 @@ class DataDictionaryController extends Controller
         return $this->get('fos_rest.view_handler')->handle($view);
     }
 
+    /**
+     * @Rest\Delete("/dictionaries/{id}", name="rednose_framework_delete_dictionary")
+     */
+    public function deleteDictionary($id)
+    {
+        $manager = $this->get('rednose_framework.data_dictionary_manager');
+        $dictionary = $manager->findDictionaryById($id);
+
+        if ($dictionary === null) {
+            throw $this->createNotFoundException();
+        }
+
+        $manager->deleteDictionary($dictionary);
+
+        return new Response(null, Codes::HTTP_NO_CONTENT);
+    }
     /**
      * @Rest\Get("/dictionaries/{id}/download", name="rednose_framework_download_dictionary", options={"expose"=true})
      */
@@ -152,8 +175,10 @@ class DataDictionaryController extends Controller
 
     /**
      * @Rest\Post("/dictionaries/upload", name="rednose_dictionary_upload", options={"expose"=true})
+     *
+     * @Rest\QueryParam(name="organization_id", nullable=false, strict=true)
      */
-    public function uploadDictionaryAction()
+    public function uploadDictionaryAction(ParamFetcherInterface $paramFetcher)
     {
         $request = $this->get('request');
         $manager = $this->get('rednose_framework.data_dictionary_manager');
@@ -163,38 +188,34 @@ class DataDictionaryController extends Controller
         $context = new DeserializationContext();
         $context->setGroups(array('file'));
 
-        $organization = $this->get('request')->get('organization');
+        $organizationManager = $this->get('rednose_framework.organization_manager');
+        $organization = $organizationManager->findOrganizationById($paramFetcher->get('organization_id'));
 
         if (!$organization) {
-            return new Response('', Codes::HTTP_BAD_REQUEST);
+            throw $this->createNotFoundException();
         }
 
-        if ($file = $request->files->get('assetFile')) {
-            if ($file->getClientOriginalExtension() === 'xml') {
-                if ($xml = file_get_contents($file->getPathname())) {
-                    if ($object = $serializer->deserialize($xml, 'Rednose\FrameworkBundle\Entity\DataDictionary', 'xml', $context)) {
-                        // Bind the dictionary to a supplied organization. Warning: existing connection will be lost.
-                        if ($organization !== null) {
-                            $organizationManager = $this->get('rednose_framework.organization_manager');
+        foreach($request->files as $uploadedFile) {
+            if ($uploadedFile instanceof UploadedFile && $uploadedFile->isValid()) {
+                $xml = file_get_contents($uploadedFile->getPathname());
 
-                            if ($organization = $organizationManager->findOrganizationById($organization)) {
-                                $err = $manager->updateDictionary($object, true);
+                $id = XmlUtil::getId($xml);
 
-                                if ($err !== true) {
-                                    return new Response((string) $err, Codes::HTTP_BAD_REQUEST);
-                                }
-
-                                $organization->setDataDictionary($object);
-
-                                $organizationManager->updateOrganization($organization);
-                            } else {
-                                return new Response('Dictionary not found.', Codes::HTTP_NOT_FOUND);
-                            }
-                        }
-
-                        return new Response($object->getId());
-                    }
+                if (!$id || $id === '') {
+                    return new Response(null, Codes::HTTP_BAD_REQUEST);
                 }
+
+                $context = new DeserializationContext();
+                $context->setGroups(array('file'));
+                $context->setAttribute('id', $id);
+
+                /** @var DataDictionaryInterface $dictionary */
+                $dictionary = $serializer->deserialize($xml, 'Rednose\FrameworkBundle\Entity\DataDictionary', 'xml', $context);
+                $dictionary->setOrganization($organization);
+
+                $manager->updateDictionary($dictionary);
+
+                return $this->redirect($this->generateUrl('rednose_framework_get_dictionary', array('id' => $dictionary->getId())));
             }
         }
 
