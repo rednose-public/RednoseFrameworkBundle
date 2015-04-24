ca5fa16e8b8b91550332cfaf65b160bee235959f
Marc Bontje <marc@rednose.nl>
2015-04-24 12:26:29 +0200

    User API routing

--- Controller/SecurityController.php
+++ Controller/SecurityController.php
@@ -11,8 +11,6 @@
 
 namespace Rednose\FrameworkBundle\Controller;
 
-use FOS\RestBundle\View\View;
-use Rednose\FrameworkBundle\Model\UserInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\Security\Core\SecurityContext;
 
@@ -58,54 +56,5 @@ class SecurityController extends Controller
     public function logoutAction()
     {
         throw new \RuntimeException('You must activate the logout in your security firewall configuration.');
-    }
-
-    /**
-     * Returns information about the current user.
-     */
-    public function getUserAction()
-    {
-        /** @var UserInterface $user */
-        $user    = $this->get('security.context')->getToken()->getUser();
-        $request = $this->getRequest();
-        $manager = $this->get('rednose_framework.organization_manager');
-
-        $locale       = $user->getLocale() ?: $request->getLocale();
-        $organization = $user->getOrganization() ?: $manager->findOrganizationBy(array());
-
-        $data = array(
-            'id'           => $user->getId(),
-            'username'     => $user->getUsername(),
-            'bestname'     => $user->getBestname(),
-            'email'        => $user->getEmail(),
-            'locale'       => $locale,
-            'organization' => $organization ? $organization->getId() : null,
-        );
-
-        $view = new View();
-        $view->setData($data);
-        $view->setFormat('json');
-
-        return $this->get('fos_rest.view_handler')->handle($view);
-    }
-
-    public function postUserAction()
-    {
-        /** @var UserInterface $user */
-        $user   = $this->get('security.context')->getToken()->getUser();
-        $params = json_decode($this->getRequest()->getContent(), true);
-
-        if (array_key_exists('locale', $params)) {
-            $user->setLocale($params['locale']);
-        }
-
-        if (array_key_exists('organization', $params)) {
-            $organization = $this->get('rednose_framework.organization_manager')->findOrganizationById($params['organization']);
-            $user->setOrganization($organization);
-        }
-
-        $this->get('rednose_framework.user_manager')->updateUser($user);
-
-        return $this->redirect($this->generateUrl('rednose_framework_get_user'));
     }
 }
