9ba10b07762e1744ba80a465aeccf27940dffd6c
Marc Bontje <marc@rednose.nl>
2015-09-01 11:35:06 +0200

    Added test cases, fixed some data model checks

--- Test/WebTestCase.php
+++ Test/WebTestCase.php
@@ -3,16 +3,20 @@
 namespace Rednose\FrameworkBundle\Test;
 
 use Doctrine\Common\DataFixtures\Purger\ORMPurger;
-use Rednose\FrameworkBundle\Model\UserInterface;
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase as BaseWebTestCase;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 
 class WebTestCase extends BaseWebTestCase
 {
+    /**
+     * @var ContainerInterface
+     */
+    protected $container;
+
     public function setUp()
     {
         $client = static::createClient();
-        $container = $client->getContainer();
+        $container = $this->container = $client->getContainer();
 
         // Purge DB
         $em = $container->get('doctrine.orm.entity_manager');
