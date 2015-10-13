28f5a7b3446246ce4f38f25b9767eb097efee57f
Marc Bontje <marc@rednose.nl>
2015-10-13 10:39:29 +0200

    Forms: Updated tests, added migration, cleanup

--- Entity/Organization.php
+++ Entity/Organization.php
@@ -42,7 +42,7 @@ class Organization extends BaseOrganization
     protected $name;
 
     /**
-     * @ORM\Column(type="string")
+     * @ORM\Column(type="string", nullable=true)
      *
      * @Serializer\Groups({"list", "details"})
      */
