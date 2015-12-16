5e0e208a3804e19981b7177e90a33e7db97ff7c8
Marc Bontje <marc@rednose.nl>
2015-12-16 16:49:14 +0100

    Framework: Fixed default role

--- Entity/User.php
+++ Entity/User.php
@@ -210,16 +210,4 @@ class User extends BaseUser implements UserInterface
     {
         return $this->getUsername() === $user->getUsername();
     }
-
-    /**
-     * @return array
-     */
-    public function getRoles()
-    {
-        return $this->roles;
-    }
-
-    public function setRoles(array $roles)
-    {
-    }
 }
