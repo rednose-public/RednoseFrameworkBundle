527a4d4accaa557a1b314c417a6236d77888e326
Sven Hagemann <sven@rednose.nl>
2016-01-08 09:58:57 +0100

    Removed old and potetialy insecure token authentication from the user manager

--- Entity/UserManager.php
+++ Entity/UserManager.php
@@ -80,56 +80,6 @@ class UserManager extends BaseUserManager implements UserManagerInterface
     }
 
     /**
-     * Attempt to authenticate using a daily logic token.
-     * This method looks in the request object for the following
-     * post or get variables: 'token' and 'username'
-     *
-     * The token is a sha1 of the sha1 username of the supplied username and
-     * a sha1 of the date (notation: 01-01-1970) followed by
-     * the secret in the parameters(.ini|.yml|.xml).
-     * Example: sha1(sha1('username') . sha1(date('d-m-Y') . 'token'));
-     *
-     * Warning: The application secret is used as a private key between application
-     * and must therefor remain secret!
-     *
-     * @param object $container
-     *
-     * @return boolean
-     *
-     * @throws \InvalidArgumentException when the secret parameter is invalid.
-     * @throws AccessDeniedException when the token is invalid.
-     */
-    public function tokenAuthentication($container)
-    {
-        $request = $container->get('request');
-        $secret = trim($container->getParameter('kernel.secret'));
-
-        if ($request->get('token') && $request->get('username')) {
-            if ($secret === '' || $secret === 'ThisTokenIsNotSoSecretChangeIt') {
-                throw new \InvalidArgumentException('Secret parameter invalid');
-            }
-
-            $token = sha1(
-                sha1($request->get('username')) .
-                sha1(date('d-m-Y') . $secret)
-            );
-
-            if ($token !== trim($request->get('token'))) {
-                throw new AccessDeniedException('Token not accepted');
-            }
-
-            $container->get('fos_user.security.login_manager')->loginUser(
-                $container->getParameter('fos_user.firewall_name'),
-                $this->loadUserByUsername($request->get('username'))
-            );
-
-            return true;
-        }
-
-        return false;
-    }
-
-    /**
      * @param string $username
      *
      * @return UserInterface
--- Model/UserManagerInterface.php
+++ Model/UserManagerInterface.php
@@ -17,25 +17,6 @@ use Symfony\Component\DependencyInjection\ContainerInterface;
 interface UserManagerInterface extends BaseUserManagerInterface
 {
     /**
-     * Attempt to authenticate using a daily logic token.
-     * This method looks in the request object for the following
-     * post or get variables: 'token' and 'username'
-     *
-     * The token is a sha1 of the sha1 username of the supplied username and
-     * a sha1 of the date (notation: 01-01-1970) followed by
-     * the secret in the parameters(.ini|.yml|.xml).
-     * Example: sha1(sha1('username') . sha1(date('d-m-Y') . 'token'));
-     *
-     * Warning: The application secret is used as a private key between application
-     * and must therefor remain secret!
-     *
-     * @param ContainerInterface $container
-     *
-     * @return boolean
-     */
-    public function tokenAuthentication($container);
-
-    /**
      * @param string $username
      *
      * @return UserInterface
