062e21bc5d9df17afdf806a938702339f934bf7b
Marc Bontje <marc@rednose.nl>
2016-01-27 13:08:58 +0100

    Blueprints: Testing

--- Behat/JavaScriptMinkContext.php
+++ Behat/JavaScriptMinkContext.php
@@ -7,6 +7,8 @@ use Behat\Mink\Exception\ElementNotFoundException;
 
 class JavaScriptMinkContext extends \Behat\MinkExtension\Context\MinkContext
 {
+    use HasAngularTrait;
+
     /**
      * {@inheritdoc}
      */
@@ -40,21 +42,5 @@ class JavaScriptMinkContext extends \Behat\MinkExtension\Context\MinkContext
     {
         $this->getSession()->getDriver()->focus($element->getXpath());
         $this->getSession()->evaluateScript('$("#'.$element->getAttribute('id').'").typeahead("val", "'.$value.'");');
-    }
-
-    protected function waitForAngular()
-    {
-        if ($this->getSession()->evaluateScript('return (typeof angular !== \'undefined\')')) {
-            $this->getSession()->evaluateScript(
-                'angular.getTestability(document.body).whenStable(function() {
-                    window.__testable = true;
-                })');
-
-            $this->getSession()->wait(5000, 'window.__testable === true');
-        }
-
-        if ($this->getSession()->evaluateScript('return (typeof jQuery != \'undefined\')')) {
-            $this->getSession()->wait(5000, '(0 === jQuery.active && 0 === jQuery(\':animated\').length)');
-        }
     }
 }
